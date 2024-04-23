d29b783373926642e01ecae0f710de0fba9b06c5
Maria Polezhaeva <mari.polezhaeva.97@mail.ru>
2024-04-23 18:03:35 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 210; // стоимость билета
+        int y = 20;
+
+        int x = amount /y;
+
+
+        System.out.println("Ваши бонусные мили:" + x);
+
+    }
+}
